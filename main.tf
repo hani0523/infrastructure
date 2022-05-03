0ddb6b03f7714d9e9c6be2b0a5bfea8cbe6debc0
hani0523 <77364371+hani0523@users.noreply.github.com>
2022-05-03 12:55:29 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,30 +1,18 @@
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+terraform {
+  backend "s3" {
+    key = "base-infrastructure/app-backend/terraform.tfstate"
+  }
 }
 
-resource "aws_lambda_function" "this" {
-  # If the file is not in the current working directory you will need to include a 
-  # path.module in the filename.
-  filename      = var.file_name
-  function_name = var.function_name
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = var.lambda_handler_name
-  source_code_hash = filebase64sha256(var.file_name)
-  runtime = var.runtime
+# Configuring the AWS Provider in us-east-2 region
+provider "aws" {
+  region = var.region
+}
+
+module "lambda-function" {
+  source        = "../../../modules/lambda"
+  file_name     = "../bin/lambda_jwt_authorizer.zip"
+  function_name = "lambda-authorizer"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
 }
--- variable.tf
+++ variable.tf
@@ -1,26 +1,7 @@
-variable "file_name" {
-  type = string
-  description = "Source zip lambda file name"
-}
+variable "region" {
 
-variable "handler" {
- type = string 
- description = "Lambda function handler"
-}
+  type        = string
+  description = "AWS Region Name"
+  default     = "us-east-1"
 
-variable "function_name" {
-  type = string
-  description = "Lambda Function Name"
-}
-
-variable "lambda_handler_name" {
-  type = string
-  description = "Handler name for the lambda function"
-  default  = "lambda_handler"
-}
-
-variable "runtime" {
-  type = string 
-  description = "Runtime to be used by the lambda function"
-  default = "python3.8"
 }
